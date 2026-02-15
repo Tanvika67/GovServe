42626d300142905af46b088c775141db850d1ee0
Bejugam <2472076@cognizant.com>
2026-02-15 14:39:41 +0530

    Added Escalation Table

---
+++ GovServe/Models/Escalation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace GovServe.Models
+{
+	public class Escalation
+	{
+		[Key]
+		public int EscalationId { get; set; }
+		[Required]
+		public int CaseId {  get; set; }
+		public int? RaisedByUserId { get; set; }
+		public string Reason { get; set; }
+		public string Status { get; set; }
+		public DateTime CreatedDate { get; set; }
+
+	}
+}
